db00f65c13999a18fe1469d639ac2297021c48f9
saharkh <skhazali99@gmail.com>
2020-06-02 21:17:17 +0430

    first commit

---
+++ app/src/main/java/com/example/organizationalapp/Font.java
@@ -0,0 +1,25 @@
+package com.example.organizationalapp;
+
+import android.app.Application;
+
+import androidx.appcompat.widget.AppCompatTextView;
+
+import com.example.organizationalapp.*;
+
+import io.github.inflationx.calligraphy3.CalligraphyConfig;
+import io.github.inflationx.calligraphy3.CalligraphyInterceptor;
+import io.github.inflationx.viewpump.ViewPump;
+public class Font extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ViewPump.init(ViewPump.builder()
+                .addInterceptor(new CalligraphyInterceptor(
+                        new CalligraphyConfig.Builder()
+                                .setDefaultFontPath("fonts/iransans.ttf")
+                                .setFontAttrId(R.attr.fontPath)
+                                .build()))
+                .build());
+        //....
+    }
+}
--- app/src/main/java/com/example/organizationalapp/MainActivity.java
+++ app/src/main/java/com/example/organizationalapp/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.organizationalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import io.github.inflationx.viewpump.ViewPumpContextWrapper;
+
+public class MainActivity extends BaseContext {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }

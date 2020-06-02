4b88039cc24e39645e442d2a265d2c94ea631058
saharkh <skhazali99@gmail.com>
2020-06-02 21:19:04 +0430

    first commit

---
+++ app/src/main/java/com/example/organizationalapp/BaseContext.java
@@ -0,0 +1,15 @@
+package com.example.organizationalapp;
+
+import android.content.Context;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import io.github.inflationx.viewpump.ViewPumpContextWrapper;
+
+public class BaseContext extends AppCompatActivity {
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(ViewPumpContextWrapper.wrap(newBase));
+    }
+}
